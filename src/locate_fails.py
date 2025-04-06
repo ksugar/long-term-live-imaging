818e5ab17b2daf9b07fe5794735725e9bc076bf7
Ko Sugawara <ko.sugawara@riken.jp>
2025-04-06 19:39:30 +0900

    Add locate_fails.py

--- src/locate_fails.py
+++ src/locate_fails.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import skimage.io
 from skimage.measure import regionprops
 from scipy.ndimage import center_of_mass
@@ -24,6 +25,26 @@ def main():
     img_dict = {}
     z_dict = {}
     p_dir = Path(args.detlog).parent
+    dict_splitting = {
+        "label": [],
+        "t": [],
+        "centroid_z": [],
+    }
+    dict_fn = {
+        "label": [],
+        "t": [],
+        "centroid_z": [],
+    }
+    dict_fp = {
+        "label": [],
+        "t": [],
+        "centroid_z": [],
+    }
+    dict_gt = {
+        "label": [],
+        "t": [],
+        "centroid_z": [],
+    }
     with open(args.detlog, "r") as file:
         for line in file:
             if line == "----------Splitting Operations (Penalty=5)----------\n":
@@ -62,13 +83,28 @@ def main():
                         gt_dict[t] = img
                     centroid = get_centroid(img, label)
                     z_dict.get(step, []).append(centroid[0] * args.scale)
+                    if step == "Splitting Operations":
+                        dict_splitting["label"].append(label)
+                        dict_splitting["t"].append(t)
+                        dict_splitting["centroid_z"].append(centroid[0] * args.scale)
+                    elif step == "False Negative Vertices":
+                        dict_fn["label"].append(label)
+                        dict_fn["t"].append(t)
+                        dict_fn["centroid_z"].append(centroid[0] * args.scale)
+                    elif step == "False Positive Vertices":
+                        dict_fp["label"].append(label)
+                        dict_fp["t"].append(t)
+                        dict_fp["centroid_z"].append(centroid[0] * args.scale)
                     print(f"Centroid of label {label} in T={t}: {centroid}")
     gt_zs = []
-    for p_gt_img in (p_dir.parent / "01_GT" / "SEG").glob("*.tif"):
+    for p_gt_img in sorted((p_dir.parent / "01_GT" / "SEG").glob("*.tif")):
         img = skimage.io.imread(p_gt_img)
         for prop in regionprops(img):
             centroid = tuple(map(float, prop.centroid))
             gt_zs.append(prop.centroid[0] * args.scale)
+            dict_gt["label"].append(prop.label)
+            dict_gt["t"].append(int(p_gt_img.stem[7:]))
+            dict_gt["centroid_z"].append(prop.centroid[0] * args.scale)
     p_output = Path(args.output)
     p_output.mkdir(exist_ok=True, parents=True)
     plt.hist(gt_zs, bins=range(0, 25, 1), label="GT")
@@ -124,6 +160,24 @@ def main():
     plt.legend()
     plt.savefig(p_output / "relative.png")
     plt.close()
+
+    # Save the data to a CSV file
+    pd.DataFrame(dict_splitting).to_csv(
+        p_output / "splitting_operations.csv",
+        index=False,
+    )
+    pd.DataFrame(dict_fn).to_csv(
+        p_output / "false_negative_vertices.csv",
+        index=False,
+    )
+    pd.DataFrame(dict_fp).to_csv(
+        p_output / "false_positive_vertices.csv",
+        index=False,
+    )
+    pd.DataFrame(dict_gt).to_csv(
+        p_output / "gt.csv",
+        index=False,
+    )
 
 
 if __name__ == "__main__":
